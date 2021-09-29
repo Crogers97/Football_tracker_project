155ebdfa8c19d9ce574b8d8de4368e0e460e1cd4
CodeClan Student <codeclanstudent@CodeClans-MacBook-Pro-2.local>
2021-09-29 12:01:01 +0100

    seventh commit

--- tests/team_tests.py
+++ tests/team_tests.py
@@ -1,5 +1,7 @@
 import unittest
 from models.team import Team
+from models.fixture import Fixture
+import repositories.fixture_repositories as fixture_repositories
 
 class TestTeam(unittest.TestCase):
 
@@ -11,3 +13,11 @@ class TestTeam(unittest.TestCase):
 
     def test_team_has_points(self):
         self.assertEquals(25, self.team.points)
+    
+
+class TestTeam(unittest.TestCase):
+
+    def setUp(self):
+        self.fixture = Fixture(team9, team7,"3-0","Home Win")
+    
+    
